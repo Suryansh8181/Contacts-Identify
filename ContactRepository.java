757fdcce5294c2b5becfd7864426d53b9a372120
Suryansh Baranwal <suryanshb123@gmail.com>
2023-11-26 13:31:27 +0530

    Create ContactRepository.java

---
+++ ContactRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    Optional<Contact> findByEmailOrPhoneNumber(String email, String phoneNumber);
+}
